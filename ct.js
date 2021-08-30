f8f8245f965965658f10d50d8cb4f06243a3dfa6
ericmday <eric.michael.day@gmail.com>
2021-08-30 15:35:05 -0700

    Update ct.js

--- ct.js
+++ ct.js
@@ -3,3 +3,19 @@ window.onload = function() {
     document.querySelector("a[href='/content']").remove();
     document.querySelector(".notion-navbar").remove();
   };
+
+    var lastScrollTop = 0;
+	$(window).scroll(function () {
+		var st = $(this).scrollTop();
+        var nav = $('.nav');
+		if (st < 100) {
+            nav.removeClass('hide');
+		} else if (st > 0 && st < $(document).height() - $(window).height()) {
+			if (st > lastScrollTop && !nav.hasClass('nav-mobile')) {
+				nav.addClass('hide');
+			} else {
+				nav.removeClass('hide');
+			}
+			lastScrollTop = st;
+		}
+	}); 
