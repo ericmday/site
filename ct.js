22463803491bf4cf171522bb8e22fa8c4d5be9af
ericmday <eric.michael.day@gmail.com>
2021-08-30 16:16:11 -0700

    Update ct.js

--- ct.js
+++ ct.js
@@ -3,3 +3,43 @@ window.onload = function() {
     document.querySelector("a[href='/content']").remove();
     document.querySelector(".notion-navbar").remove();
 };
+
+$('.nav-toggle').click(function() {
+    $(this).toggleClass('nav-toggled');
+    $('.nav-links').toggleClass('nav-links-show');
+    var width = $(window).width();
+    if (width <= 480) {
+        $('.nav').toggleClass('nav-mobile');
+    } else {
+
+    }
+});
+
+$(document).keydown(function(e) {
+    if (e.key === 'Escape') {
+        $('.nav-toggle').removeClass('nav-toggled');
+        $('.nav-links').removeClass('nav-links-show');
+        var width = $(window).width();
+        if (width <= 480) {
+            $('.nav').removeClass('nav-mobile');
+        } else {
+
+        }
+    }
+});
+
+var lastScrollTop = 0;
+$(window).scroll(function() {
+    var st = $(this).scrollTop();
+    var nav = $('.nav');
+    if (st < 100) {
+        nav.removeClass('hide');
+    } else if (st > 0 && st < $(document).height() - $(window).height()) {
+        if (st > lastScrollTop && !nav.hasClass('nav-mobile')) {
+            nav.addClass('hide');
+        } else {
+            nav.removeClass('hide');
+        }
+        lastScrollTop = st;
+    }
+});
